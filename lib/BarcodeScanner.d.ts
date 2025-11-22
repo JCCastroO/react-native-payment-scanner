eec00852116df726f8578fd84b084056c8f7f4c5
joao.oliveira <joao.oliveira@opah.com.br>
2025-11-22 19:24:08 -0300

    add

---
+++ lib/BarcodeScanner.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+import { ScannerProps } from "./types";
+export declare const BarcodeScanner: React.FC<ScannerProps>;
---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export { BarcodeScanner } from "./BarcodeScanner";
+export type { ScanResult, BarcodeType, ScannerProps } from "./types";
